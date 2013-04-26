6fbf3ea57f27785a7cf6ff2e9089b04d26efb6f3
--replace-all <qf26@cornell.edu>
2013-04-26 13:25:25 -0400

    change the url

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    var socket = io.connect('http://localhost');
+    //var socket = io.connect('http://localhost');
+    
+    var socket = io.connect('http://desolate-lake-6924.herokuapp.com');
     socket.on('update', function (data) {
       $("#chat_text").append('<li><span style="color:' + data.color + '; padding:2px; margin-right:5px; border:1px solid ' + 
         data.color + '"> ' + data.user + '</span>' + data.msg + '</li>');
